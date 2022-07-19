39fbbc261c39908be02833e6e7962e3e61b5bf1b
Greg Munro <gregmunro@Websy-Greg.local>
2022-07-19 17:14:07 +0200

    bar chart

--- index.js
+++ index.js
@@ -3,9 +3,23 @@ const svg = d3.select('body')
               .attr('height', 500)
               .attr('width', 800)
 
-svg.append('rect')
-  .attr('width', 300)
-  .attr('height', 200)
-  .attr('x', 50)
-  .attr('y', 50)
-  .style('fill', '#ffaa00')
+const data = [
+                {category: "a", value: 100},
+                {category: "b", value: 200},
+                {category: "c", value: 50},
+                {category: "d", value: 150},
+                {category: "e", value: 250},
+                {category: "f", value: 100}
+            ]
+
+const boxes = svg.selectAll('rect')
+                .data(data)
+                .enter()
+                .append('rect')
+                .attr('width', 100)
+                .attr('height', d => {
+                  return d.value
+                })
+                .attr('x', (d, i) => {
+                  return i * 110
+                })
